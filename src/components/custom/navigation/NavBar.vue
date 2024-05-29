eeffb660a90849ee7e766e7d4cf20d360e493a15
OwlRepo <romeoangeles010517@gmail.com>
2024-05-29 23:11:17 +0800

    feat: increase lighthouse rating by added aria-label

--- src/components/custom/navigation/NavBar.vue
+++ src/components/custom/navigation/NavBar.vue
@@ -61,10 +61,13 @@ const currentRoute = computed(() => window.location.pathname);
               <Search
                 class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground"
               />
+              <label for="search"></label>
               <Input
+                name="search"
                 type="search"
                 placeholder="Search products..."
-                class="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+                aria-label="search"
+                class="w-full appearance-none bg-background pl-8 shadow-none"
               />
             </div>
           </form>
