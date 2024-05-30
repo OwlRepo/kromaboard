36962ef5de26b0eb5e60c887b10da4d8bd3cb7a7
OwlRepo <romeoangeles010517@gmail.com>
2024-05-31 00:34:57 +0800

    fix: fix warnings

--- src/components/custom/form/DateRangePicker.vue
+++ src/components/custom/form/DateRangePicker.vue
@@ -33,38 +33,40 @@ const value = ref({
 </script>
 
 <template>
-  <Popover>
-    <PopoverTrigger as-child>
-      <Button
-        variant="outline"
-        :class="
-          cn(
-            'w-full justify-start text-left font-normal overflow-clip',
-            !value && 'text-muted-foreground'
-          )
-        "
-      >
-        <CalendarIcon class="mr-2 h-4 w-4" />
-        <template v-if="value.start">
-          <template v-if="value.end">
-            {{ df.format(value.start.toDate(getLocalTimeZone())) }} -
-            {{ df.format(value.end.toDate(getLocalTimeZone())) }}
-          </template>
+  <div>
+    <Popover>
+      <PopoverTrigger as-child>
+        <Button
+          variant="outline"
+          :class="
+            cn(
+              'w-full justify-start text-left font-normal overflow-clip',
+              !value && 'text-muted-foreground'
+            )
+          "
+        >
+          <CalendarIcon class="mr-2 h-4 w-4" />
+          <template v-if="value.start">
+            <template v-if="value.end">
+              {{ df.format(value.start.toDate(getLocalTimeZone())) }} -
+              {{ df.format(value.end.toDate(getLocalTimeZone())) }}
+            </template>
 
-          <template v-else>
-            {{ df.format(value.start.toDate(getLocalTimeZone())) }}
+            <template v-else>
+              {{ df.format(value.start.toDate(getLocalTimeZone())) }}
+            </template>
           </template>
-        </template>
-        <template v-else> Pick a date </template>
-      </Button>
-    </PopoverTrigger>
-    <PopoverContent class="w-auto p-0">
-      <RangeCalendar
-        v-model="value"
-        initial-focus
-        :number-of-months="2"
-        @update:start-value="(startDate) => (value.start = startDate)"
-      />
-    </PopoverContent>
-  </Popover>
+          <template v-else> Pick a date </template>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent class="w-auto p-0">
+        <RangeCalendar
+          v-model="value"
+          initial-focus
+          :number-of-months="2"
+          @update:start-value="(startDate) => (value.start = startDate)"
+        />
+      </PopoverContent>
+    </Popover>
+  </div>
 </template>
