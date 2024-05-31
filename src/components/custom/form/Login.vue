4b091d0a698e25f7c7e1257ca618a33ed1ce1cb2
OwlRepo <romeoangeles010517@gmail.com>
2024-06-01 00:00:18 +0800

    feat: initial sign up form

--- src/components/custom/form/Login.vue
+++ src/components/custom/form/Login.vue
@@ -5,58 +5,41 @@ import { Label } from "@/components/ui/label";
 </script>
 
 <template>
-  <div class="w-full lg:grid min-h-[100vh] lg:grid-cols-2">
-    <div class="flex items-center justify-center py-12">
-      <div class="mx-auto grid w-[350px] gap-6">
-        <div class="grid gap-2 text-center">
-          <h1 class="text-3xl font-bold">Login</h1>
-          <p class="text-balance text-muted-foreground">
-            Enter your email below to login to your account
-          </p>
-        </div>
-        <div class="grid gap-4">
-          <div class="grid gap-2">
-            <Label for="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div class="grid gap-2">
-            <div class="flex items-center">
-              <Label for="password">Password</Label>
-              <a
-                href="/forgot-password"
-                class="ml-auto inline-block text-sm underline"
-              >
-                Forgot your password?
-              </a>
-            </div>
-            <Input id="password" type="password" required />
-          </div>
-          <Button type="submit" class="w-full"> Login </Button>
-          <Button variant="outline" class="w-full"> Login with Google </Button>
-        </div>
-        <div class="mt-4 text-center text-sm">
-          Don't have an account?
-          <a href="#" class="underline"> Sign up </a>
-        </div>
-        <div class="text-center text-sm">
-          Want to see how it works?
-          <a href="/dashboard" class="underline"> See preview </a>
-        </div>
+  <div class="flex items-center justify-center py-12">
+    <div class="mx-auto grid w-[350px] gap-6">
+      <div class="grid gap-2 text-center">
+        <h1 class="text-3xl font-bold">Login</h1>
+        <p class="text-balance text-muted-foreground">
+          Enter your email below to login to your account
+        </p>
       </div>
-    </div>
-    <div class="hidden bg-muted lg:block">
-      <img
-        src="/assets/img/login_banner.webp"
-        alt="Image"
-        width="1920"
-        height="1080"
-        class="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-      />
+      <div class="grid gap-4">
+        <div class="grid gap-2">
+          <Label for="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div class="grid gap-2">
+          <div class="flex items-center">
+            <Label for="password">Password</Label>
+            <a
+              href="/forgot-password"
+              class="ml-auto inline-block text-sm underline"
+            >
+              Forgot your password?
+            </a>
+          </div>
+          <Input id="password" type="password" required />
+        </div>
+        <Button type="submit" class="w-full"> Login </Button>
+      </div>
+      <div class="mt-4 text-center text-sm">
+        Don't have an account?
+        <a href="/sign-up" class="underline"> Sign up </a>
+      </div>
+      <div class="text-center text-sm">
+        Want to see how it works?
+        <a href="/dashboard" class="underline"> See preview </a>
+      </div>
     </div>
   </div>
 </template>
