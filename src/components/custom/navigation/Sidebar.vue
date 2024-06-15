24094a8bd6cac3b70bc5366ab0658b0bf9c7074d
OwlRepo <romeoangeles010517@gmail.com>
2024-06-16 03:10:28 +0800

    feat: basic implementation of product tour guide

--- src/components/custom/navigation/Sidebar.vue
+++ src/components/custom/navigation/Sidebar.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { LOGGED_IN_ROUTES } from "@/constants";
+import ProductTour from "../ProductTour.vue";
 const currentRoute = window.location.pathname;
 </script>
 
 <template>
   <nav class="grid items-start px-2 text-sm font-medium lg:px-4">
+    <ProductTour />
     <a
       v-for="route in LOGGED_IN_ROUTES"
       :key="route.path"
@@ -15,6 +17,7 @@ const currentRoute = window.location.pathname;
         'text-muted-foreground hover:text-foreground':
           !route.path.includes(currentRoute),
       }"
+      :id="route.name.toLowerCase().replace('/', '')"
     >
       <component
         :is="route.icon"
