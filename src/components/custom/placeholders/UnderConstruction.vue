65953145ee3440b967c19ea079d7080800384bbc
OwlRepo <romeoangeles010517@gmail.com>
2024-06-16 01:54:04 +0800

    feat: under construction placeholder

---
+++ src/components/custom/placeholders/UnderConstruction.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-gray-100 h-full flex items-center justify-center">
+    <div class="flex flex-col items-center text-center">
+      <iframe
+        src="https://lottie.host/embed/93344bfc-14a3-4c28-a6ca-2d439df6405e/WYZtiD5q2R.json"
+        class="w-96 h-96"
+      />
+      <h1 class="text-4xl font-bold text-gray-900 mb-4">
+        Page Under Construction 🚧
+      </h1>
+      <p class="text-lg text-gray-800 mb-8">
+        We're working hard to bring you something awesome.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
